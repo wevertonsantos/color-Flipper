d39c7bf40d1f539de2dcd92758469efa2e720e1c
Weverton Santos <wever-santos@hotmail.com>
2022-09-06 18:26:35 -0300

    Tentando fazer o random para gerar as cores

--- js/script.js
+++ js/script.js
@@ -2,5 +2,26 @@ let btn = document.querySelector('.btn')
 
 btn.addEventListener('click', function(){
     document.body.style.backgroundColor = 'blue'
-
 })
+
+color = [
+    '#F1f5f8'
+]
+
+function random(min,max){
+    return Math.random() * (max - min) + min
+}
+
+
+console.log(random())
+
+// Utilizado no projeto
+
+// arrays
+// document.getElementById()
+// document.querySelector()
+// addEventListener()
+// document.body.style.backgroundColor
+// Math.floor()
+// Math.random()
+// array.length
