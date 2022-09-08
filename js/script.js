86000844d6acc7a740e8de108243697bc6f8e89a
Weverton Santos <wever-santos@hotmail.com>
2022-09-08 19:36:02 -0300

    Add innerHTML para mostrar o bg e função color

--- js/script.js
+++ js/script.js
@@ -1,19 +1,20 @@
 let btn = document.querySelector('.btn')
-
-btn.addEventListener('click', function(){
-    document.body.style.backgroundColor = 'blue'
-})
-
-color = [
-    '#F1f5f8'
+let mostrarColor = document.getElementById('printColor')
+let randomColor = color = [
+    gerar_cor_hexa(),
 ]
 
-function random(min,max){
-    return Math.random() * (max - min) + min
+
+btn.addEventListener('click', function(){
+    document.body.style.backgroundColor = randomColor
+    mostrarColor.innerHTML = gerar_cor_hexa()
+})
+
+function gerar_cor_hexa(){
+    return ' : #' + parseInt((Math.random() * 0xFFFFFF))
+    .toString(16)
+    .padStart(6,'0')
 }
-
-
-console.log(random())
 
 // Utilizado no projeto
 
